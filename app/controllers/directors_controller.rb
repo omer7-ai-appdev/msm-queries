aa406f2fa112968e933c36eb72f2211c2e66e93d
Omero Arreola <omarreola16@gmail.com>
2022-10-31 17:11:13 +0000

    the youngest director has been added

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,21 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+
+    render({ :template => "director_templates/index.html.erb" })
+  end
+
+  def wisest 
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+
+    render({ :template => "director_templates/eldest.html.erb" })
+  end
+
+    def greenest
+      @youngest = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
+
+    render({ :template => "director_templates/youngest.html.erb" })
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get("/directors/eldest", { :controller => 
+  "directors", :action => "wisest" })
+
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors/", { :controller => :"directors", :action => "index" })
+
+  get("/directors/youngest", { :controller => 
+  "directors", :action => "greenest" })
+
 end
