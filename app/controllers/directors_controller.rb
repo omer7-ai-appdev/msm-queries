c5c37e8ab17bc51c9431d6a2511f41721cd95efb
Omero Arreola <omarreola16@gmail.com>
2022-11-02 19:58:42 +0000

    Specific director bio, dob, specific movies directed by

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -26,6 +26,9 @@ class DirectorsController < ApplicationController
         
         @the_director = Director.where({ :id => the_id }).at(0)
 
+        @filmography = Movie.where({ :director_id => @the_director.id })
+
+
       render({ :template => "director_templates/show.html.erb" })
     end
 
