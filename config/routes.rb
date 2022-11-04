73d4953771a829bdadf972868dfcd83b230cd580
Omero Arreola <omarreola16@gmail.com>
2022-11-04 20:28:54 +0000

    up to 55.56%. think ive already committed this but just in case

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  # user visits /directors/
 
   get("/", { :controller => "application", :action => "homepage" }) 
+
+  
+  get("/directors/youngest", { :controller => 
+  "directors", :action => "greenest" })
 
   get("/directors/eldest", { :controller => 
   "directors", :action => "wisest" })
 
-  get("/directors/youngest", { :controller => 
-  "directors", :action => "greenest" })
-
   
-
+  
   get("/directors/", { :controller => :"directors", :action => "index" })
 
   get("/directors/:an_id", { :controller => 
@@ -20,6 +22,10 @@ Rails.application.routes.draw do
 
   get("/movies/", { :controller => :"movies", :action => "index" })
 
-  get("/movies/:an_id", { :controller => :"movies", :action => "show"})
+  get("/movies/:an_id", { :controller => 
+  "movies", :action => "show" })
+
+
+
 
 end
